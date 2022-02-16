894a759e54b31e8cceb15e0f825af5c5d6982068
Arighna Chakraborty <riju23chakra@gmail.com>
2022-02-16 23:22:16 +0530

    added .env support

--- config/default.ts
+++ config/default.ts
@@ -2,5 +2,5 @@ import 'dotenv/config'
 
 export default {
     port: 1337,
-    dbUrl: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@rest-api-node.ynhpw.mongodb.net`,
+    dbUrl: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_NAME}.ynhpw.mongodb.net`,
 }
